76d7132593cd1c06c4a99a43ea3810a1a5a00371
Your Name <you@example.com>
2023-09-28 10:33:24 +0400

    make changes in roles and created artisan command for creating service

---
+++ app/Console/Commands/MakeService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Console\Command;
+
+class MakeService extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new service class';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $className = $name;
+        $filePath = app_path('Services') . '/' . $className . '.php';
+
+        if (File::exists($filePath)) {
+            $this->error('Service class already exists!');
+            return;
+        }
+
+        $namespace = 'App\Services';
+
+        // Create the directory if it doesn't exist
+        File::ensureDirectoryExists(app_path('Services'));
+
+        // Create the service class file with the namespace and an empty class
+        File::put($filePath, "<?php\n\nnamespace $namespace;\n\nclass $className\n{\n   //\n}\n");
+
+        $this->info("Service class $className created successfully!");
+
+        return Command::SUCCESS;
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,12 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
+    use HasRoles;
     /**
      * Seed the application's database.
      *
@@ -17,10 +20,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'admin@admin.com',
             'password' => Hash::make('156354111')
-        ]);
+        ])->assignRole('admin');
     }
 }
