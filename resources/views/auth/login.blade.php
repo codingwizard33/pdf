71f721c9d06a0bc7a3ab1fafe7a1583a7e116a42
codingwizard33 <codingwizard33@gmail.com>
2023-10-04 11:32:03 +0400

    Fixed bugs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }} - Reset Password</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>6670 - PDF edit detection software</title>
 
-    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dist/css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('dist/css/app.css') }}">
 
-</head>
-<body class="sidebar-mini layout-fixed layout-navbar-fixed">
-<div class="wrapper">
+    </head>
+    <body class="sidebar-mini layout-fixed layout-navbar-fixed">
+        <div class="wrapper">
 
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <span class="text-danger">6670 - PDF edit detection software</span>
-    </div>
+            <!-- Preloader -->
+            <div class="preloader flex-column justify-content-center align-items-center">
+                <span class="text-danger">6670 - PDF edit detection software</span>
+            </div>
 
-    <div class="container-fluid">
-        <div class="row d-flex justify-content-center align-items-center h_100">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header h5 text-white text-center bg-info">Reset Password</div>
-                    <div class="card-body px-5">
-                        <form action="">
-                            @csrf
-                            <div class="form-outline">
-                                <label class="form-label" for="new_password">New Password</label>
-                                <input type="password" id="new_password" class="form-control mb-3" required />
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center align-items-center h_100">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2>Login</h2>
+                                <h3 class="mb-3">to get started</h3>
+                                <form action="{{ route('login') }}" method="post">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input id="login" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" onkeyup="success()" placeholder="Login" required autofocus>
+
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" name="password" class="form-control" placeholder="Password" id="pas" onkeyup="success()" required/>
+                                    </div>
+
+                                    <a href="">Forgot Password?</a>
+
+                                    <input type="submit" value="Continue" class="btn btn-success w-100 mt-3 mb-3" id="continue" disabled>
+
+                                    {{-- <div class="text-center">
+                                        <span>New User? <a href="">Register</a></span>
+                                    </div> --}}
+                                </form>
                             </div>
-                            <div class="form-outline">
-                                <label class="form-label" for="confirm_password">Confirm Password</label>
-                                <input type="password" id="confirm_password" class="form-control mb-3" required />
-                            </div>
-                            <button type="submit" class="btn btn-success w-100">Reset password</button>
-                        </form>
-                        <div class="d-flex justify-content-between mt-4">
-                            <a class="btn btn-outline-info" href="{{ route('login') }}">Login</a>
-                            <a class="btn btn-outline-info" href="#">Register</a>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
-    </div>
 
-</div>
-
-<!-- jQuery -->
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
-<script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-<script src="{{ asset('dist/js/adminlte.js') }}"></script>
-<script src="{{ asset('dist/js/app.js') }}"></script>
-</body>
+        <!-- jQuery -->
+        <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+        <script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+        <script src="{{ asset('dist/js/adminlte.js') }}"></script>
+        <script src="{{ asset('dist/js/app.js') }}"></script>
+    </body>
 </html>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }} - Forgot Password</title>
+    <title>{{ config('app.name') }} - Reset Password</title>
 
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
@@ -25,16 +25,17 @@
         <div class="row d-flex justify-content-center align-items-center h_100">
             <div class="col-md-4">
                 <div class="card">
-                    <div class="card-header h5 text-white text-center bg-info">Forgot Password</div>
+                    <div class="card-header h5 text-white text-center bg-info">Reset Password</div>
                     <div class="card-body px-5">
-                        <p class="card-text py-2 text-center">
-                            Enter your email address and we'll send you an email with instructions to reset your password.
-                        </p>
                         <form action="">
                             @csrf
                             <div class="form-outline">
-                                <label class="form-label" for="typeEmail">Email</label>
-                                <input type="email" id="typeEmail" class="form-control mb-3" required />
+                                <label class="form-label" for="new_password">New Password</label>
+                                <input type="password" id="new_password" class="form-control mb-3" required />
+                            </div>
+                            <div class="form-outline">
+                                <label class="form-label" for="confirm_password">Confirm Password</label>
+                                <input type="password" id="confirm_password" class="form-control mb-3" required />
                             </div>
                             <button type="submit" class="btn btn-success w-100">Reset password</button>
                         </form>
