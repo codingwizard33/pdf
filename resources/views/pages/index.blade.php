641b5ce996c909257bdbbbded9deee5205775c39
codingwizard33 <codingwizard33@gmail.com>
2023-10-05 09:53:37 +0400

    Change Overview

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,7 +10,7 @@
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Gallery Overfiew</a></li>
+                            <li class="breadcrumb-item"><a href="#">Overview</a></li>
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                     </div>
